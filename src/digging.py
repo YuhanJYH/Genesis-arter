57c5c5595a84c8f6c52866927442050c45552f5a
Yuhan Jin <yuhan.jin@dfki.de>
2025-04-29 10:56:28 +0200

    up

--- src/digging.py
+++ src/digging.py
@@ -1,10 +1,12 @@
 import numpy as np
 import torch
 import gymnasium as gym
-# import rsl_rl 
+
+# import rsl_rl
 
 from scipy.spatial.transform import Rotation as R
 import math
+
 # motion planning
 import pybullet
 import pybullet_data
@@ -18,6 +20,7 @@ import helpers
 import arter
 
 import genesis as gs
+
 gs.init(backend=gs.gpu)
 
 N_ENVS = 100
@@ -42,10 +45,10 @@ scene = gs.Scene(
         camera_fov=40,
         max_FPS=100,
     ),
-    vis_options=gs.options.VisOptions(
-        show_world_frame=True,
-        ambient_light=(0.5, 0.5, 0.5),
-    ),
+    # vis_options=gs.options.VisOptions(
+    #     show_world_frame=True,
+    #     # ambient_light=(0.5, 0.5, 0.5),
+    # ),
     # rigid_options=gs.options.RigidOptions(
     #     enable_joint_limit=True,
     #     gravity=[0.0, 0.0, -9.81],
@@ -56,11 +59,11 @@ scene = gs.Scene(
 
 if RECORD:
     cam = scene.add_camera(
-    res= (2160, 1440),
-    pos=(0, 50, 50),
-    lookat=(0, 25, 5),
-    fov=90,
-    GUI=False,
+        res=(2160, 1440),
+        pos=(0, 50, 80),
+        lookat=(0, 25, 5),
+        fov=60,
+        GUI=False,
     )
 
 
@@ -72,10 +75,10 @@ platform = scene.add_entity(
     #     fixed=True,
     # ),
     gs.morphs.Mesh(
-        file='/workspace/genesis/assets/meshes/platform.obj',
-        decimate = False,
-        convexify = False,
-        quality = False,
+        file="/workspace/genesis/assets/meshes/platform.obj",
+        decimate=False,
+        convexify=False,
+        quality=False,
         collision=True,
         visualization=True,
         pos=(0, 0, 0),
@@ -92,14 +95,8 @@ platform = scene.add_entity(
 #     )
 # )
 arter_entity = scene.add_entity(
-    gs.morphs.URDF(
-        file=arter.URDF_PATH, 
-        pos=INIT_POS, 
-        quat=INIT_QUAT,
-        fixed=False,
-        merge_fixed_links=False  # 0.4551
-    ),
-    visualize_contact=False,    
+    gs.morphs.URDF(file=arter.URDF_PATH, pos=INIT_POS, quat=INIT_QUAT, fixed=False, merge_fixed_links=False),  # 0.4551
+    visualize_contact=False,
 )
 ############################ genesis idx ##########################
 swiviel_jnts_idx = [arter_entity.get_joint(name).dof_idx_local for name in arter.swiviel_jnts]
@@ -113,14 +110,14 @@ chasis_jnts_idx = swiviel_jnts_idx + stabilizer_jnts_idx + claw_jnts_idx
 manipulator_jnts_idx = [arter_entity.get_joint(name).dof_idx_local for name in arter.manipulator_jnts]
 whole_jnts_idx = manipulator_jnts_idx + chasis_jnts_idx + wheel_jnts_idx + steerring_jnts_idx
 
-print('friction wheel:',arter_entity.get_link("arter/wheel_fl_link").geoms[0].friction)
+print("friction wheel:", arter_entity.get_link("arter/wheel_fl_link").geoms[0].friction)
 
 # Set the friction coefficient for the wheel links
 for wheel_link_name in wheel_link_names:
     wheel_link = arter_entity.get_link(wheel_link_name)
     wheel_link.set_friction(friction_coefficients["road"]["dry_asphalt_concrete"]["min"])  # Set to min value
-    
-print('friction wheel:',arter_entity.get_link("arter/wheel_fl_link").geoms[0].friction)
+
+print("friction wheel:", arter_entity.get_link("arter/wheel_fl_link").geoms[0].friction)
 
 ########################### set motion planner ##########################
 # pybullet.connect(pybullet.DIRECT)  # Connect in GUI-less mode
@@ -160,15 +157,24 @@ print('friction wheel:',arter_entity.get_link("arter/wheel_fl_link").geoms[0].fr
 
 if START_SIM:
     scene.build(n_envs=N_ENVS, env_spacing=(ENV_SPACING, ENV_SPACING))
-    
+
     ########################### set init state ##################
     manipulator_init_pos = [0, 1, 0.5, 0, 0, 0, 0]
+    manipulator_init_pos = np.array(manipulator_init_pos)
     manipulator_init_pos = np.tile(manipulator_init_pos, (N_ENVS, 1))
+    random_mask = np.random.uniform(low=-0.5, high=0.5, size=(N_ENVS, 7))
+    manipulator_init_pos = manipulator_init_pos + random_mask
     arter_entity.set_dofs_position(manipulator_init_pos, manipulator_jnts_idx)
+    print("randomized manipulator init pos: ", manipulator_init_pos)
 
     chasis_init_pos = np.zeros(len(chasis_jnts_idx))
     chasis_init_pos[4:8] = [-0.2, -0.2, -0.2, -0.2]
     chasis_init_pos = np.tile(chasis_init_pos, (N_ENVS, 1))
+    rand = [np.random.uniform(low=-0.2, high=0.0)]*4
+    random_mask = np.array(rand)
+    random_mask = np.tile(random_mask, (N_ENVS, 1))
+    chasis_init_pos[:, 4:8] = chasis_init_pos[:, 4:8] + random_mask
+    
     arter_entity.set_dofs_position(chasis_init_pos, chasis_jnts_idx)
 
     ################# set robot controller ##################
@@ -192,7 +198,7 @@ if START_SIM:
     iter = 0
     if RECORD:
         cam.start_recording()
-        iter = int(1e3)
+        iter = int(1e4)
     else:
         iter = int(1e8)
 
@@ -205,7 +211,7 @@ if START_SIM:
         wheel_vel = np.array([3 * np.pi] * 4)
         wheel_vel = wheel_vel_mask * wheel_vel
         wheel_vel = np.tile(wheel_vel, (N_ENVS, 1))
-        
+
         steer_vel = np.array([0, 0, 0, 0])
         steer_vel = np.tile(steer_vel, (N_ENVS, 1))
 
@@ -213,22 +219,21 @@ if START_SIM:
         arter_entity.control_dofs_velocity(manipulator_vel, manipulator_jnts_idx)
         arter_entity.control_dofs_velocity(wheel_vel, wheel_jnts_idx)
         arter_entity.control_dofs_velocity(steer_vel, steerring_jnts_idx)
-        
+
         scene.step()
         if RECORD:
             cam.render(depth=False)
-        
+
         base_pos = arter_entity.get_pos()
         base_quat = arter_entity.get_quat()
 
-        
         # reset to init at tipping
         for i in range(N_ENVS):
             # if arter_entity.get_pos()[i][2] < 5.2 or arter_entity.get_pos()[i][2] > 6.5:
             env_quat = base_quat[i]
             env_quat = env_quat.cpu()
             env_quat = np.array(env_quat)
-            print("env_quat: ", env_quat)
+            # print("env_quat: ", env_quat)
             # general tilt angle > 30 degrees
             r1 = R.from_quat(env_quat)
             r_init = R.from_quat(INIT_QUAT)
@@ -236,8 +241,8 @@ if START_SIM:
             rot_vector = r_diff.as_rotvec()
             diff_angle = np.linalg.norm(rot_vector)
             diff_angle = math.degrees(diff_angle)
-            print("diff_angle: ", diff_angle)
-            if diff_angle > 40:
+            print("diff_angle ", i, ": ", diff_angle)
+            if diff_angle > 40 or base_pos[i][2] < 5.2 or base_pos[i][2] > 6.5:
                 print("arter is tipping, reset to init")
                 # reset to init
                 for j in range(len(base_pos[i])):
@@ -246,6 +251,6 @@ if START_SIM:
                 for j in range(len(base_quat[i])):
                     base_quat[i][j] = INIT_QUAT[j]
                 arter_entity.set_quat(base_quat)
-    
-    if RECORD:            
+
+    if RECORD:
         cam.stop_recording(save_to_filename="arter_drop.mp4", fps=60)
